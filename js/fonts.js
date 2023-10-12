016e0915f069581edcf3b6e58c3d02ba57be3e7f
shopvn62 <147729101+shopvn62@users.noreply.github.com>
2023-10-13 00:37:50 +0700

    Update fonts.js

--- js/fonts.js
+++ js/fonts.js
@@ -5,25 +5,6 @@ function confirmAndRedirect() {
       window.location.href = "https://m.me/lemin62";
     }
   }
-	// messenger live 
-	var chatbox = document.getElementById('fb-customer-chat');
-      chatbox.setAttribute("page_id", "2374897302799565");
-      chatbox.setAttribute("attribution", "biz_inbox");
-
-window.fbAsyncInit = function() {
-        FB.init({
-          xfbml            : true,
-          version          : 'v18.0'
-        });
-      };
-
-      (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));
 	
       // text code lemin.vn 
 document.addEventListener("DOMContentLoaded", function() {

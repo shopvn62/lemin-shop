aed9d91b352ea29cdfbd934f4ded7fe48a13d065
shopvn62 <147729101+shopvn62@users.noreply.github.com>
2023-10-15 17:55:52 +0700

    Delete js directory

--- js/size.js
+++
@@ -1,5 +0,0 @@
-function changePrice(newPrice) {
-            var priceElement = document.getElementById("price");
-            priceElement.innerHTML = newPrice + ",000 ₫"; // Thay đổi giá trị văn bản
-            priceElement.classList.add("highlight"); // Thêm class để định dạng nếu cần
-        }
